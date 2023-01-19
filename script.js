4cfdd43a1904e93d2d126ff79cb49d10e666beab
Trishan Wagle <mailtotrishan@gmail.com>
2023-01-19 18:37:25 +0545

    fix: fixed all errors and bugs

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let addTaskBtn, userInput, checkBox, deleteBtn
 const taskDiv = document.querySelector('.addedTasks')
 
 
+// updates Local Storage
 const updateLS = () => {
     localStorage.setItem('tasks', JSON.stringify(tasksArr));
 }
 let tasksArr = JSON.parse(localStorage.getItem('tasks'));
 
+// creates an input field and a button for the user to add a task.
 const fetchAddTask = () => {
     let ihtml = `
     <div class = "addTask">
@@ -21,63 +23,36 @@ const fetchAddTask = () => {
     addTaskBtn.addEventListener('click', fetchTask)
 }
 
-const refresh = () => {
-    tasksArr.forEach((task) => {
-        taskDiv.innerHTML += `
-            <div class = "todo">
-            <p class = "task"> ${task} </p>
-            <img src = "check.png" class = "check func">
-            <img src = "delete.png" class = "delete func">
-            </div>
-            `
-        addSection.innerHTML = `<button class="task-input-button">Add Task</button>`
-        taskInputButton = document.querySelector('.task-input-button')
-        taskInputButton.addEventListener('click', fetchAddTask)
-
-        checkBox = document.querySelectorAll('.check')
-        checkBox.forEach((check) => {
-            check.addEventListener('click', () => {
-                let element = check.previousElementSibling
-                element.classList.toggle('stroke')
-            })
-        })
-
-        deleteBtn = document.querySelectorAll('.delete')
-        deleteBtn.forEach((btn) => {
-            btn.addEventListener('click', () => {
-                let elementParent = btn.parentElement
-                let elementText = elementParent.firstElementChild.textContent.trim()
-                elementParent.remove()
-                let index = tasksArr.indexOf(elementText)
-                tasksArr.splice(index, 1)
-                updateLS()
-            })
-        })
-    })
-}
-refresh()
-
-
+// fetch Tasks in the Tasks Section
 const fetchTask = () => {
     userInput = document.querySelector('.taskArea')
     tasksArr.push(userInput.value)
     updateLS()
+    addTaskToList()
+}
+
+// adds task to the task list
+const addTaskToList = () => {
     taskDiv.innerHTML += `
-            <div class = "todo">
-            <p class = "task"> ${userInput.value} </p>
-            <img src = "check.png" class = "check func">
-            <img src = "delete.png" class = "delete func">
-            </div>
-            `
+    <div class = "todo">
+    <p class = "task"> ${userInput.value} </p>
+    <img src = "images/check.png" class = "check func">
+    <img src = "images/delete.png" class = "delete func">
+    </div>
+    `
     addSection.innerHTML = `<button class="task-input-button">Add Task</button>`
     taskInputButton = document.querySelector('.task-input-button')
     taskInputButton.addEventListener('click', fetchAddTask)
+    checkAndDelete()
+}
 
+// delete and check functions
+const checkAndDelete = () => {
     checkBox = document.querySelectorAll('.check')
     checkBox.forEach((check) => {
         check.addEventListener('click', () => {
             let element = check.previousElementSibling
-            element.classList.toggle('stroke')
+            element.classList.toggle('line-through')
         })
     })
 
@@ -94,5 +69,23 @@ const fetchTask = () => {
     })
 }
 
+// fetching the tasks even after refreshing
+const refresh = () => {
+    tasksArr.forEach((task) => {
+        taskDiv.innerHTML += `
+            <div class = "todo">
+            <p class = "task"> ${task} </p>
+            <img src = "images/check.png" class = "check func">
+            <img src = "images/delete.png" class = "delete func">
+            </div>
+            `
+        addSection.innerHTML = `<button class="task-input-button">Add Task</button>`
+        taskInputButton = document.querySelector('.task-input-button')
+        taskInputButton.addEventListener('click', fetchAddTask)
+
+        checkAndDelete()
+    })
+}
+refresh()
 
 taskInputButton.addEventListener('click', fetchAddTask)
